de898971d71c50a94add2c99e615d29f5d2ab155
Pankaj Patil <pankajhanmant.patil@sjsu.edu>
2019-10-22 13:56:37 -0700

    Creating POST upload API

--- routes/upload.js
+++ routes/upload.js
@@ -1,11 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require("aws-sdk");
+const multer = require("multer");
+const config = require("../config");
+
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
 
 
-// Sample
+// GET to upload
 router.get('/', function (req, res) {
     console.log("GET API /upload called!");
     res.status(200).send({message: "Got result from GET"});
 });
+
+
+// POST to upload
+router.post("/", upload.single("file"), function (req, res) {
+    const file = req.file;
+
+
+    let s3bucket = new AWS.S3({
+        accessKeyId: config.AWS_ACCESS_KEY_ID,
+        secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+        region: config.AWS_REGION
+    });
+
+    //Location of store for file upload
+
+    var params = {
+        Bucket: config.AWS_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: "public-read"
+    };
+
+    s3bucket.upload(params, function (err, data) {
+        if (err) {
+            res.status(500).json({error: true, Message: err});
+        } else {
+            //success
+            res.send({data});
+
+            //insert in database
+            var newFileUploaded = {
+                description: req.body.description,
+                fileLink: s3FileURL + file.originalname,
+                s3_key: params.Key
+            };
+
+            console.log(newFileUploaded);   
+
+        }
+    });
+});
+
 
 module.exports = router;
