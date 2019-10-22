80259d83d15e738c24529d22cbf1c61982ce261e
Pankaj Patil <pankajhanmant.patil@sjsu.edu>
2019-10-21 22:42:42 -0700

    started with RestFul APIs

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express(),
-path = require('path'),
-publicDir = path.join(__dirname,'public');
+    path = require('path'),
+    publicDir = path.join(__dirname, 'public');
+const router = express.Router();
 
 app.use(express.static(publicDir))
 
+
 app.listen(port);
+
+// Sample
+app.get('/sample', function (req, res) {
+    console.log("GET API /Sample called!");
+    res.status(200).send({message: "Got result from GET"});
+});
+
 module.exports = app;
