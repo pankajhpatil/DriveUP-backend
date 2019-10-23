7ecc7bdbc7886401a69bc87436823d8e1a7c8205
Pankaj Patil <pankajhanmant.patil@sjsu.edu>
2019-10-22 17:14:45 -0700

    Created API for fetching  Bucket Objects

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var app = express(),
     publicDir = path.join(__dirname, 'public');
 const router = express.Router();
 var upload = require('./routes/upload')
+var download = require('./routes/fetch')
 
 
 app.use(express.static(publicDir))
 
 
 app.use('/upload', upload);
+app.use('/download', download);
 
 app.listen(port);
 
