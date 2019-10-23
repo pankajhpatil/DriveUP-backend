ed1be31a61065eb8cff68cdc9312f8d6cca3ce49
pankajhpatil <55374522+pankajhpatil@users.noreply.github.com>
2019-10-23 04:59:50 -0700

    RDS integration

--- routes/upload.js
+++ routes/upload.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var AWS = require("aws-sdk");
 const multer = require("multer");
 const config = require("../config");
+var mysql = require('./db/sql');
 
 var storage = multer.memoryStorage();
 var upload = multer({storage: storage, limits: {fileSize: 10 * 1024 * 1024}});
@@ -37,6 +38,8 @@ router.post("/", upload.single("file"), function (req, res) {
         ACL: "public-read"
     };
 
+    
+
     s3bucket.upload(params, function (err, data) {
         if (err) {
             res.status(500).json({error: true, Message: err});
@@ -57,5 +60,79 @@ router.post("/", upload.single("file"), function (req, res) {
     });
 });
 
+router.get('/test', function (req, res) {
+
+    console.log("INside /test");
+
+    var sqlQuery = "SELECT * from user_data";
+
+
+    
+        mysql.fetchData(function (err, results) {
+            if (err) {
+                throw err;
+            }
+            else {
+
+                console.log("Fetch Project Details Successful!");
+                res.statusMessage = "Data fetched";
+                res.status(200).send({result: results});
+
+            }
+        }, sqlQuery);
+    
+  
+
+});
+
+router.post('/insert', function (req, res, next) {
+    var user_id = req.file;
+    console.log(user_id);
+    //console.log("INside /insert" +  "withoutbody" +req.body);
+
+
+    // var sqlQuery = "INSERT INTO `user_data` (`user_id`, `username`, `password`, `firstname`, `lastname`) VALUES ('" + req.body.user_id + "','" + req.body.username + "', '" + req.body.password + "', '" + req.body.firstname + "', '" + req.body.lastname + "')";
+
+    
+    //     mysql.fetchData(function (err, results) {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         else {
+
+    //             console.log("Insert Complete");
+    //             res.statusMessage = "Insert Complete";
+    //             res.status(200).send({result: results});
+
+    //         }
+    //     }, sqlQuery);
+    
+  
+
+});
+
+router.get('/getuser', function (req, res) {
+
+    console.log("INside /getuser" +  "withoutbody" +req.body+res.body+req.user_id);
+
+    var sqlQuery = "select * from user_data where user_id=req.body.user_id or username=req.body.username";
+
+    
+        mysql.fetchData(function (err, results) {
+            if (err) {
+                throw err;
+            }
+            else {
+
+                console.log("Insert Complete");
+                res.statusMessage = "Insert Complete";
+                res.status(200).send({result: results});
+
+            }
+        }, sqlQuery);
+    
+  
+
+});
 
 module.exports = router;
