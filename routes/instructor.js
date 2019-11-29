9220267f5cb4291a58c3fbce8729a9fa3a75802b
Pankaj Patil <55374522+pankajhpatil@users.noreply.github.com>
2019-11-28 16:28:19 -0800

    added page to display instructor schedule

--- routes/instructor.js
+++ routes/instructor.js
@@ -5,7 +5,7 @@ const multer = require("multer");
 const config = require("../config");
 var mysql = require('./db/sql');
 var moment = require('moment');
-const instructorSchedule= require('./Models/instructorSchedule');
+const instructorschedule= require('./models/instructorschedule');
 
 
 
@@ -13,11 +13,26 @@ var storage = multer.memoryStorage();
 var upload = multer({storage: storage, limits: {fileSize: 10 * 1024 * 1024}});
 
 
+router.get('/getISchedule', function (req, res) {
+//instructorschedules
+var name=req.session.username;
+  
+instructorschedule.find({ iusername : name },(err, data) => {
+    console.log(data);
+    res.statusMessage = "Fetch Complete";
+    res.status(200).send({result: data});
+});
+
+});
+
+
 // GET to upload
 router.get('/', function (req, res) {
-    console.log("Show instructor schedules!");
+    console.log("insert instructor schedules!");
+    var username=req.session.username;
+    username="test22";
     var now = moment();
-    var later = moment(now, "DD-MM-YYYY").add(30, 'days');
+    var later = moment(now, "DD-MM-YYYY").add(7, 'days');
 
     var sDate=now.format("DD-MM-YYYY");
     var eDate=later.format("DD-MM-YYYY");
@@ -27,17 +42,14 @@ router.get('/', function (req, res) {
     console.log(later.diff(now,"days"));
     for(var i=0; i<=later.diff(now,"days"); i++){
         //console.log(now.format("DD-MM-YYYY"));
-        now = moment(now, "DD-MM-YYYY").add(1, 'days');
-
-    }
-     //mongo
-     const ISchedule = new instructorSchedule({
-        instructorID : 2,
-        sdate : now,
+          //mongo
+     const ISchedule = new instructorschedule({
+        iusername : username,
+        sdate : now.format("DD-MMM-YYYY"),
         slot0810:"Y",
         slot1012:"Y",
-        slot1214:"Y",
-        slot1416:"Y",
+        slot1214:"N",
+        slot1416:"N",
         slot1618:"Y",
         slot1820:"Y",
         slot2022:"N"
@@ -50,6 +62,10 @@ router.get('/', function (req, res) {
           console.log('Schedule registered in Mongo');
       })
       .catch(err=>console.log(err));
+
+    now = moment(now, "DD-MM-YYYY").add(1, 'days');
+    }
+   
 
       //{"_id":{"$oid":"5ddf7d6e1c9d440000467c4e"},"instructorID":"1","sdate":{"$date":{"$numberLong":"1573459200000"}},"slot0810":"Y","slot1012":"Y","slot1214":"Y","slot1416":"Y","slot1618":"Y","slot1820":"Y","slot2022":"Y"}
         res.status(200).send({message: "Got result from GET"});
@@ -83,26 +99,17 @@ router.post('/updateIdetails', function (req, res, next) {
 
 });
 
-router.post('/deleteIdetails', function (req, res, next) {
+router.post('/deleteISdetails', function (req, res, next) {
 
+//instructorschedules
+var name=req.session.username;
+var deletiondate=req.body.sdate;  
 
-    var sqlQuery = "delete from `dropboxmysql`.`user_data`   WHERE (`username` = '" + req.body.username + "')";
-
-    console.log(sqlQuery);
-
-    mysql.fetchData(function (err, results) {
-        if (err) {
-            throw err;
-        }
-        else {
-
-            console.log("Record deleted Complete");
-            res.statusMessage = "Delete Complete";
-            res.status(200).send({result: results});
-
-        }
-    }, sqlQuery);
-
+instructorschedule.deleteOne({ iusername : name ,sdate:deletiondate },(err, data) => {
+    console.log(data);
+    res.statusMessage = "delete Complete";
+    res.status(200).send({result: data});
+});
 
 });
 
